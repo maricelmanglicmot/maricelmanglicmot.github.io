77b079ef5c5ec360572b83a25426923046ffc5af
maricelmanglicmot <90233262+maricelmanglicmot@users.noreply.github.com>
2021-10-12 23:36:30 -0700

    font color change

--- ist541/lab6/js/hotspot.js
+++ ist541/lab6/js/hotspot.js
@@ -3,20 +3,20 @@ $( function() {
    $("map area").click( function(){
 
       if ($(this).attr("id") == "boop") {
-         $("#feedback").css("color","green");
+         $("#feedback").css("color","saddlebrown");
          $("#feedback").html("Boop! You have booped Manini's nose.");
       }
 	 if ($(this).attr("id") == "blep") {
-         $("#feedback").css("color","green");
+         $("#feedback").css("color","saddlebrown");
          $("#feedback").html("Blep... Manini sticks his tongue out at you.");
       }
       if ($(this).attr("id") == "headrub") {
-         $("#feedback").css("color","green");
+         $("#feedback").css("color","saddlebrown");
          $("#feedback").html("You gave Manini a head rub!");
       }
 
       if ($(this).attr("id") == "paw") {
-         $("#feedback").css("color","red");
+         $("#feedback").css("color","saddlebrown");
          $("#feedback").html("Grr! Manini doesn't like having his paws touched!");
       }
 
