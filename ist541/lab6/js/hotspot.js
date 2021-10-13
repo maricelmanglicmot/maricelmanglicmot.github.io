a637510be9661ecaba963c7c30f923a793d96fff
maricelmanglicmot <90233262+maricelmanglicmot@users.noreply.github.com>
2021-10-12 23:47:21 -0700

    Update hotspot.js

--- ist541/lab6/js/hotspot.js
+++ ist541/lab6/js/hotspot.js
@@ -3,20 +3,20 @@ $( function() {
    $("map area").click( function(){
 
       if ($(this).attr("id") == "boop") {
-         $("#feedback").css("color","saddlebrown");
+         $("#feedback").css("color","black");
          $("#feedback").html("Boop! You have booped Manini's nose.");
       }
 	 if ($(this).attr("id") == "blep") {
-         $("#feedback").css("color","saddlebrown");
+         $("#feedback").css("color","black");
          $("#feedback").html("Blep... Manini sticks his tongue out at you.");
       }
       if ($(this).attr("id") == "headrub") {
-         $("#feedback").css("color","saddlebrown");
+         $("#feedback").css("color","black");
          $("#feedback").html("You gave Manini a head rub!");
       }
 
       if ($(this).attr("id") == "paw") {
-         $("#feedback").css("color","saddlebrown");
+         $("#feedback").css("color","black");
          $("#feedback").html("Grr! Manini doesn't like having his paws touched!");
       }
 
